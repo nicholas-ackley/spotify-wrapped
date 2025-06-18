08d39f9b7ea945dec870cfb222dcfc7f80ed5c86
Nicholas Ackley <nicholasackley@Nicholass-MacBook-Pro-2.local>
2025-06-18 15:59:28 -0500

    first

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
-=======
-export default defineConfig({
-  plugins: [react()],
-  base: '/spotify-wrapped/', // <-- add this line
+  base: '/spotify-wrapped/',
+  
 })
->>>>>>> d2a1a1e91596cd2820ce43fef98327b72672483a
